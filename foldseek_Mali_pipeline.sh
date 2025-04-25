21dede0a6f6016b46eab755cd065d83572c345ca
georgedashen <zhuoyang2019@gmail.com>
2025-04-25 17:16:42 +0800

    foldseek Mali exhaustive-search

--- foldseek_Mali_pipeline.sh
+++ foldseek_Mali_pipeline.sh
@@ -10,9 +10,9 @@ foldseek createdb MalisamPDB/ database/Malisam
 foldseek createdb MalidupPDB/ database/Malidup
 
 mkdir Malidup_result
-foldseek easy-search database/Malidup database/Malidup Malidup_result/aln.tsv tmpFolder --format-output "query,target,qaln,taln"
+foldseek easy-search --exhaustive-search database/Malidup database/Malidup Malidup_result/aln.tsv tmpFolder --format-output "query,target,qaln,taln"
 cp Malidup_result/aln.tsv Malidup_foldseek.tsv
 
 mkdir Malisam_result
-foldseek easy-search database/Malisam database/Malisam Malisam_result/aln.tsv tmpFolder --format-output "query,target,qaln,taln"
+foldseek easy-search --exhaustive-search database/Malisam database/Malisam Malisam_result/aln.tsv tmpFolder --format-output "query,target,qaln,taln"
 cp Malisam_result/aln.tsv Malisam_foldseek.tsv
